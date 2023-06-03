5b6a9769b6a9548970e5a43ebff57f73e37f9c4d
vishalpadaswan3 <vishalpadaswan1.com>
2023-06-03 14:24:16 +0530

    backend only

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const {connection} = require('./config/db');
-const {userRouter} = require('./Routes/user.Routes');
-const {classiRouter} = require('./Routes/classi.routes');
+const { connection } = require('./config/db');
+const { userRouter } = require('./Routes/user.Routes');
+const { classiRouter } = require('./Routes/classi.routes');
 
 const cors = require('cors');
 require('dotenv').config();
@@ -10,6 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
